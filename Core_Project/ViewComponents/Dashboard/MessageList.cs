3fbe654a6b60296ce8ea2191157d937653e735af
mcelik99 <mehmetcelik.99@outlook.com>
2023-08-07 21:46:30 +0300

    Admin Tarafı Son Düzenlemeler

--- Core_Project/ViewComponents/Dashboard/MessageList.cs
+++ Core_Project/ViewComponents/Dashboard/MessageList.cs
@@ -2,14 +2,17 @@
 using DataAccesLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Core_Project.ViewComponents.Dashboard
 {
     public class MessageList : ViewComponent
     {
+        MessageManager messageManager = new MessageManager(new EfMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            var values = messageManager.TGetList().Take(5).ToList();
+            return View(values);
         }
     }
 }
